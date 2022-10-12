5db6122f21b62add03722d9f7aff6a4899b257be
Max <0665232@mail.ru>
2022-10-12 22:30:06 +0300

    Решена задача 21

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿//Задача 25
-//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿//Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.Write("Введите число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int i = Convert.ToInt32(Math.Pow(a, b));
-Console.WriteLine(i);
+// Console.Write("Введите число A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int i = Convert.ToInt32(Math.Pow(a, b));
+// Console.WriteLine(i);
+
+
+
+//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.Write("Введите X1: ");
+// double X1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// double Y1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите Z1: ");
+// double Z1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// double X2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// double Y2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите Z2: ");
+// double Z2 = Convert.ToDouble(Console.ReadLine());
+// double R = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
+// Console.WriteLine(R);
+
